63010ed2ccb356a65758831871fa0422127c5b2e
James Gorlick <jgorlick@basho.com>
2015-08-14 10:24:15 -0700

    fixed spark-master bind ip to use last address from `hostname -I` or SPARK_MASTER_IP likely set from /etc/profile.

--- priv/extras_templates/spark/run.sh
+++ priv/extras_templates/spark/run.sh
@@ -6,9 +6,20 @@ for i in logs work; do
 done
 
 if [ "$MASTER_URL" == "" ]; then
-    export HOST=${HOST:-0.0.0.0}
+    HOST=${HOST:-0.0.0.0} #<< bind any
+    if [[ "$HOST" == "" || "$HOST" == "0.0.0.0" ]]; then
+        # hostname -I #<< yields ips similar to /sbin/ifconfig
+        HN=$(hostname -I)
+        HNA=($HN)
+        # take last address
+        HNL=${#HNA[@]}
+        HOST=${HNA[HNL-1]}
+    fi
+    export HOST
     export LEAD_ELECT_SERVICE_HOSTS=${LEAD_ELECT_SERVICE_HOSTS:-"$HOST:5323"}
     export RIAK_HOSTS=${RIAK_HOSTS:-"$HOST:8087"}
+    # if HOST based IP is not preferable, set SPARK_MASTER_IP with the correct
+    # value per machine in the appropriate profile script, ie /etc/profile
     export SPARK_MASTER_IP=${SPARK_MASTER_IP:-"$HOST"}
     export SPARK_MASTER_PORT=${SPARK_MASTER_PORT:-7077}
     export SPARK_MASTER_WEBUI_PORT=${SPARK_MASTER_WEBUI_PORT:-8080}
